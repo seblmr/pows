2321690ba4b11c304c081567b976464bc4215ea5
seblmr <smouria@gmx.fr>
2026-05-29 11:34:11 +0200

    fonts renewd and fixed

--- scripts/get-fonts.sh
+++ scripts/get-fonts.sh
@@ -5,12 +5,12 @@ mkdir -p public/fonts
 
 echo "Downloading Cormorant Light..."
 curl -fL \
-  "https://raw.githubusercontent.com/googlefonts/cormorant/main/fonts/ttf/Cormorant-Light.ttf" \
+  "https://github.com/CatharsisFonts/Cormorant/blob/master/fonts/ttf/Cormorant-Light.ttf" \
   -o public/fonts/Cormorant-Light.ttf
 
 echo "Downloading JetBrains Mono Regular..."
 curl -fL \
-  "https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/fonts/ttf/JetBrainsMono-Regular.ttf" \
+  "https://github.com/JetBrains/JetBrainsMono/blob/master/fonts/ttf/JetBrainsMono-Regular.ttf" \
   -o public/fonts/JetBrainsMono-Regular.ttf
 
 echo "Verifying fonts..."
