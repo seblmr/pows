7a557b58c42eb1f493e00f29ac61d457227c1323
seblmr <smouria@gmx.fr>
2026-05-29 11:06:26 +0200

    fonts renewed

--- scripts/get-fonts.sh
+++ scripts/get-fonts.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-# Run this once from the project root to download fonts for Satori
-# Usage: bash scripts/get-fonts.sh
+set -e
 
 mkdir -p public/fonts
 
-echo "Downloading Cormorant Garamond Light..."
-curl -L "https://github.com/googlefonts/cormorant/raw/main/fonts/ttf/Cormorant-Light.ttf" \
+echo "Downloading Cormorant Light..."
+curl -fL \
+  "https://raw.githubusercontent.com/googlefonts/cormorant/main/fonts/ttf/Cormorant-Light.ttf" \
   -o public/fonts/Cormorant-Light.ttf
 
 echo "Downloading JetBrains Mono Regular..."
-curl -L "https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-Regular.ttf" \
+curl -fL \
+  "https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/fonts/ttf/JetBrainsMono-Regular.ttf" \
   -o public/fonts/JetBrainsMono-Regular.ttf
 
-echo "Done. Fonts saved in public/fonts/"
+echo "Verifying fonts..."
+file public/fonts/Cormorant-Light.ttf
+file public/fonts/JetBrainsMono-Regular.ttf
+
+echo "Done."
